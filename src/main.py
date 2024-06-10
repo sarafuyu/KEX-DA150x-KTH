afb1d2f8c613932bc4072c3f81ef587f90a68903
Noah Hopkins <nhopkins@kth.se>
2024-06-10 16:52:12 +0200

    Add parameter space setup for tolerance plots

--- src/main.py
+++ src/main.py
@@ -181,7 +181,7 @@ STOP_AFTER_FEATURE_SELECTION: bool = False
 # Use precomputed XGB selected data dict
 # If set, will skip: cleaning, adding imputer objects, adding indicators, categorizing y, splitting data
 # and will start with imputing the data using the precomputed imputer objects.
-PRECOMPUTED_XGB_SELECTED_DATA: Path | None = None  # PROJECT_ROOT/'data'/'results'/'IterativeImputer-RFR-tol-00175-iter-98-cutoff-17-Age-GridSearch-tol-0001-FINAL-poly-detailed-accuracy'/'2024-05-27-073726__FeatureSelect꞉XGB-RFE-CV_dataset_dict.pkl'  #None  # PROJECT_ROOT/'data'/'results'/'XGB-RFECV-binlog-feat-select-num-est꞉ALL'/'2024-05-11-041302__FeatureSelect__XGB-RFE-CV_dataset_dict.pkl'  # 'XGB-RFECV-binlog-feat-select-num-est꞉ALL-Cut꞉17-Age'/'2024-05-17-154927__dataset_dict.pkl'
+PRECOMPUTED_XGB_SELECTED_DATA: Path | None = PROJECT_ROOT/'data'/'results'/'final-results'/'2024-06-08-014208__FeatureSelect꞉XGB-RFE-CV_dataset_dict.pkl'  #None  # PROJECT_ROOT/'data'/'results'/'XGB-RFECV-binlog-feat-select-num-est꞉ALL'/'2024-05-11-041302__FeatureSelect__XGB-RFE-CV_dataset_dict.pkl'  # 'XGB-RFECV-binlog-feat-select-num-est꞉ALL-Cut꞉17-Age'/'2024-05-17-154927__dataset_dict.pkl'
 
 # 37 specific features were found to be the best number of features using XGB feature selection.
 
@@ -257,8 +257,8 @@ VERBOSE_ITER_IMP: int = 3
 # Use precomputed imputed dataset
 # Leave as None to use the imputed dataset generated in the pipeline
 # Note that PRECOMPUTED_ITERATIVE_IMPUTED_DF is the full un-imputed dataset that was used to generate the imputed dataset
-PRECOMPUTED_ITERATIVE_IMPUTED_X_DATA: Path | None = None  # PROJECT_ROOT / 'data' / 'results' / 'IterativeImputer-RFR-tol-00175-iter-98-cutoff-17-Age-GridSearch-tol-0001-FINAL-poly-detailed-accuracy' / '2024-05-27-073726__IterativeImputer_X_imputed.csv'
-PRECOMPUTED_ITERATIVE_IMPUTED_DF: Path | None = None  # PROJECT_ROOT / 'data' / 'results' / 'IterativeImputer-RFR-tol-00175-iter-98-cutoff-17-Age-GridSearch-tol-0001-FINAL-poly-detailed-accuracy' / '2024-05-27-073726__IterativeImputer_df.csv'
+PRECOMPUTED_ITERATIVE_IMPUTED_X_DATA: Path | None = PROJECT_ROOT / 'data' / 'results' / 'final-results' / '2024-06-08-014208__IterativeImputer_X_imputed.csv'
+PRECOMPUTED_ITERATIVE_IMPUTED_DF: Path | None = None  # PROJECT_ROOT / 'data' / 'results' / 'final-result' / '2024-06-08-014208__IterativeImputer_df.csv'
 
 # Stop the pipeline after imputation
 STOP_AFTER_IMPUTATION: bool = False
@@ -299,7 +299,7 @@ def identity(x): return x
 # Normalization modes to try in Grid search. Can be 'None', 'StandardScaler', 'MinMaxScaler'.
 NORMALIZATION_MODES_PARAMS = [
     StandardScaler(copy=False, with_mean=True, with_std=True),
-    # MinMaxScaler(feature_range=(0, 1), copy=False, clip=False),
+    MinMaxScaler(feature_range=(0, 1), copy=False, clip=False),
     # FunctionTransformer(identity, validate=True), # No normalization
 ]
 
@@ -338,23 +338,23 @@ N_JOBS_GRID_SEARCH: int = 7
 SVC = True
 
 # Hyperparameters:            # np.logspace(start, stop, num=50)
-C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([
-    np.logspace(start=-9, stop=5, num=7+5+1, base=10),  # 10^-7 to 10^5  # TODO: larger space
-    np.logspace(start=-9, stop=-8, num=5, base=10)[1:-1],
-    np.logspace(start=-8, stop=-7, num=5, base=10)[1:-1],
-    np.logspace(start=-7, stop=-6, num=5, base=10)[1:-1],
-    np.logspace(start=-6, stop=-5, num=5, base=10)[1:-1],
-    np.logspace(start=-5, stop=-4, num=5, base=10)[1:-1],
-    np.logspace(start=-4, stop=-3, num=5, base=10)[1:-1],
-    np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
-    np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
-    np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
-    np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
-    np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
-    np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
-    np.logspace(start=3, stop=4, num=5, base=10)[1:-1],
-    np.logspace(start=4, stop=5, num=5, base=10)[1:-1],
-])))
+# C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([
+#     np.logspace(start=-9, stop=5, num=9+5+1, base=10),  # 10^-7 to 10^5  # TODO: larger space
+#     np.logspace(start=-9, stop=-8, num=5, base=10)[1:-1],
+#     np.logspace(start=-8, stop=-7, num=5, base=10)[1:-1],
+#     np.logspace(start=-7, stop=-6, num=5, base=10)[1:-1],
+#     np.logspace(start=-6, stop=-5, num=5, base=10)[1:-1],
+#     np.logspace(start=-5, stop=-4, num=5, base=10)[1:-1],
+#     np.logspace(start=-4, stop=-3, num=5, base=10)[1:-1],
+#     np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
+#     np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
+#     np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
+#     np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
+#     np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
+#     np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
+#     np.logspace(start=3, stop=4, num=5, base=10)[1:-1],
+#     np.logspace(start=4, stop=5, num=5, base=10)[1:-1],
+# ])))
 # C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([  # TODO: little bit larger
 #     np.logspace(start=-7, stop=5, num=7+5+1, base=10),  # 10^-7 to 10^5
 #     np.logspace(start=-6, stop=-7, num=5, base=10)[1:-1],
@@ -380,43 +380,62 @@ C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([
 #     np.logspace(start=3, stop=4, num=5, base=10)[1:-1],
 #     np.logspace(start=4, stop=5, num=5, base=10)[1:-1],
 # ])))
-# C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([  # TODO: smaller prestudy space
-#     np.logspace(start=-8, stop=5, num=8+5+1, base=10),  # 10^-7 to 10^5
-#     # np.logspace(start=-8, stop=2, num=8 + 2 + 1, base=10),  # 10^-7 to 10^5
-# ])))
-KERNEL_PARAMS_SVC: Sequence[str] = ['poly', 'rbf', 'sigmoid']  # 'linear', 'rbf', 'precomputed'
-DEGREE_PARAMS_SVC: Sequence[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-GAMMA_PARAMS_SVC: Sequence[str | float] = ['scale', 0, 0.01, 0.1, 1.0, 10.0]  # , 'auto', 0.01, 0.1, 1.0]  # scale not needed since normalization X_var
-COEF0_PARAMS_SVC = sorted(np.unique(np.concatenate([
-    -np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
-    -np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
-    -np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
-    -np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
-    -np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
-    -np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
-    -np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # -10^-3 to -10^3
-    np.linspace(0, 0, 1),              # 0
-    np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # 10^-3 to  10^3
-    np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
-    np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
-    np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
-    np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
-    np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
-    np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
+C_PARAMS_SVC: Sequence[float] = sorted(np.unique(np.concatenate([  # TODO: smaller prestudy space
+    # np.logspace(start=-8, stop=5, num=8+5+1, base=10),  # 10^-8 to 10^5
+    np.logspace(start=-8, stop=2, num=8 + 2 + 1, base=10),  # 10^-8 to 10^2
 ])))
-# COEF0_PARAMS_SVC = sorted(np.unique(np.concatenate([  # TODO: smaller prestudy space
+KERNEL_PARAMS_SVC: Sequence[str] = ['poly']  # 'linear', 'rbf', 'precomputed'
+# DEGREE_PARAMS_SVC: Sequence[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]  # TODO: large final space
+DEGREE_PARAMS_SVC: Sequence[int] = [4]
+# GAMMA_PARAMS_SVC: Sequence[str | float] = [0.00001, 0.0001, 0.001, 'scale', 0.01, 0.1, 1.0, 10.0, 100.0]  # , 'auto', 0.01, 0.1, 1.0]  # TODO: final large space
+GAMMA_PARAMS_SVC: Sequence[str | float] = ['scale', 0.01, 0.1, 1.0]  # , 'auto', 0.01, 0.1, 1.0]  # scale not needed since normalization X_var
+# COEF0_PARAMS_SVC = sorted(np.unique(np.concatenate([  # TODO: final large space
+#     -np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
+#     -np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
+#     -np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
+#     -np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
+#     -np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
+#     -np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
 #     -np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # -10^-3 to -10^3
 #     np.linspace(0, 0, 1),              # 0
 #     np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # 10^-3 to  10^3
-#     # -np.logspace(start=-3, stop=0, num=3+0+1, base=10),  # -10^-3 to -10^3
-#     # np.linspace(0, 0, 1),              # 0
-#     # np.logspace(start=-3, stop=2, num=3+2+1, base=10),  # 10^-3 to  10^3
+#     np.logspace(start=-3, stop=-2, num=5, base=10)[1:-1],
+#     np.logspace(start=-2, stop=-1, num=5, base=10)[1:-1],
+#     np.logspace(start=-1, stop=0, num=5, base=10)[1:-1],
+#     np.logspace(start=0, stop=1, num=5, base=10)[1:-1],
+#     np.logspace(start=1, stop=2, num=5, base=10)[1:-1],
+#     np.logspace(start=2, stop=3, num=5, base=10)[1:-1],
 # ])))
+COEF0_PARAMS_SVC = sorted(np.unique(np.concatenate([  # TODO: smaller prestudy space
+    -np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # -10^-3 to -10^3
+    np.linspace(0, 0, 1),              # 0
+    np.logspace(start=-3, stop=3, num=3+3+1, base=10),  # 10^-3 to  10^3
+    # -np.logspace(start=-3, stop=0, num=3+0+1, base=10),  # -10^-3 to -10^3
+    # np.linspace(0, 0, 1),              # 0
+    # np.logspace(start=-3, stop=2, num=3+2+1, base=10),  # 10^-3 to  10^3
+])))
 SHRINKING_PARAMS_SVC: Sequence[bool] = [True]
 PROBABILITY_SVC: Sequence[bool] = [False]
-TOL_PARAMS_SVC: Sequence[float] = [0.001]  # [0.01, 0.1, 1.0, 10.0, 100.0]  # np.linspace(0.01, 0.0001, 10)  # np.linspace(0.01, 0.0001, 10)
+# TOL_PARAMS_SVC: Sequence[float] = [0.001]  # [0.01, 0.1, 1.0, 10.0, 100.0]  # np.linspace(0.01, 0.0001, 10)  # np.linspace(0.01, 0.0001, 10)
+TOL_PARAMS_SVC = [
+    0.001,
+    0.01,
+    0.1,
+    1.0,
+    2.0,
+    3.0,
+    4.0,
+    5.0,
+    6.0,
+    7.0,
+    8.0,
+    9.0,
+    10.0,
+    100.0,
+    1000.0
+]
 CACHE_SIZE_PARAMS_SVC: Sequence[int] = [500]
-CLASS_WEIGHT_PARAMS_SVC: Sequence[str | None] = ['balanced']
+CLASS_WEIGHT_PARAMS_SVC: Sequence[str | None] = ['balanced', None]
 VERB_SVC: int = VERBOSE
 MAX_ITER_PARAMS_SVC: Sequence[int] = [10_000_000]  # [-1]  # TODO CHANGE BACK TO 10M for new prestudy
 DECISION_FUNCTION_PARAMS_SVC: Sequence[str] = ['ovr']  # only ovo if multi class
